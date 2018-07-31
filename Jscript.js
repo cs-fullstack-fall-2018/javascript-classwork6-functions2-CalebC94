8aad27c26670a3f5ed61aea6b99922efd7269919
Caleb Canada <calebcanada13@gmail.com>
2018-07-31 13:41:18 -0500

    Check in

--- Jscript.js
+++ Jscript.js
@@ -1,6 +1,18 @@
 function main() {
     var favoriteNumber = prompt("What is your favorite number?");
     alert(favoriteNumber);
+    aboveOrBelow(favoriteNumber);
 }
 
-main()
+function aboveOrBelow(favoriteNumber) {
+    if (favoriteNumber < 100) {
+        alert(favoriteNumber + " is below 100");
+    }
+    else if (favoriteNumber > 100) {
+        alert(favoriteNumber + " is above 100");
+    }
+    else {
+        alert("It's 100");
+    }
+}
+    main()
